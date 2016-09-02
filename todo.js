afd39e8426dd48cebdd347c5fa76afe5d4b0a8fe
hiepnm <hiepnm2909@gmail.com>
2016-09-02 20:38:22 +0700

    Re-implement combineReducers

--- todo.js
+++ todo.js
@@ -40,7 +40,20 @@ const visibilityFilter = (
       return state;
   }
 }
-const { combineReducers } = Redux;
+const combineReducers = (reducers) => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key) => {
+        nextState[key] = reducers[key](
+          state[key],
+          action
+        );
+        return nextState;
+      },
+      {}
+    );
+  }
+}
 const todoApp = combineReducers({
   todos,
   visibilityFilter
